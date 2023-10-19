d03a77a73b0110d35826a4baf8ffa1bab0a8513a
ShuYuHuang <b123767195@gmail.com>
2023-10-19 11:28:56 +0400

    add default values for parameters

--- app/params.py
+++ app/params.py
@@ -1,6 +1,16 @@
 # Supported File Types
-DOCUMENT_FILE_TYPES = ["txt", "pdf"]
+DOCUMENT_FILE_TYPES = ["txt", "pdf", "docx"]
 SHEET_FILE_TYPES = ["csv", "xls", "xlsx"]
 MEDIA_FILE_TYPES = ["mp4", "avi", "mov", "mkv", "mp3", "wav","m4a"]
 IMAGE_FILE_TYPES = ["jpg", "jpeg", "png", "bmp", "tiff"]
 ALL_FILE_TYPES = DOCUMENT_FILE_TYPES + SHEET_FILE_TYPES + MEDIA_FILE_TYPES + IMAGE_FILE_TYPES
+
+DEFAULT_LLM_ARGS = dict(
+    model_name='gpt-3.5-turbo',
+    temperature=0,
+    max_tokens=1000
+)
+
+DEFAULT_INDEX_NAME = "myindex"
+DEFAULT_ES_URL = "http://localhost:9200"
+DEFAULT_TMP_DIR = "tmp"
