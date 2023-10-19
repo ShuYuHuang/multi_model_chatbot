dda6234d58cda6d786eec3d1a075df768d5acd16
ShuYuHuang <b123767195@gmail.com>
2023-10-19 11:26:36 +0400

    test shell script

--- chatbot.sh
+++ chatbot.sh
@@ -1,1 +1,5 @@
-python3 -m streamlit run ./app/gui.py --server.fileWatcherType none
+streamlit run app/gui.py  --server.fileWatcherType none
+
+# For Testing
+# streamlit run app/es_usage.py  --server.fileWatcherType none
+# streamlit run app/file_processor.py  --server.fileWatcherType none
