0ae0665ec75f88d6dc0c5e41bdc32754489374a8
ShuYuHuang <b123767195@gmail.com>
2023-10-24 21:14:33 +0400

    migrate parameters from ChatGPT to Local Vicuna

--- app/params.py
+++ app/params.py
@@ -5,10 +5,16 @@ MEDIA_FILE_TYPES = ["mp4", "avi", "mov", "mkv", "mp3", "wav","m4a"]
 IMAGE_FILE_TYPES = ["jpg", "jpeg", "png", "bmp", "tiff"]
 ALL_FILE_TYPES = DOCUMENT_FILE_TYPES + SHEET_FILE_TYPES + MEDIA_FILE_TYPES + IMAGE_FILE_TYPES
 
+# DEFAULT_LLM_ARGS = dict(
+#     model_name='gpt-3.5-turbo',
+#     temperature=0,
+#     max_tokens=1000
+# )
+
 DEFAULT_LLM_ARGS = dict(
-    model_name='gpt-3.5-turbo',
-    temperature=0,
-    max_tokens=1000
+    model_name='vicuna-13b-v1.5-16k',
+    temperature=0.0,
+    max_tokens=512
 )
 
 DEFAULT_INDEX_NAME = "myindex"
